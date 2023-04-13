6c9e7e1d728724bb9a1c1a31a643f6b6d0ac6bbd
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-04-13 16:39:56 +0200

    feat(body): js

---
+++ main.js
@@ -0,0 +1,13 @@
+let distanza = prompt("Quanti km devi fare?");
+
+let età = prompt("Quanti anni hai?");
+
+let prezzoViaggio = Math.round(distanza * 0.21);
+
+if (età < 18) {
+    prezzoViaggio -= prezzoViaggio / 100 * 20;
+} else if (età > 65) {
+    prezzoViaggio -= prezzoViaggio / 100 * 40;
+}
+
+document.getElementById("prezzoViaggio").innerText = prezzoViaggio + " €";
